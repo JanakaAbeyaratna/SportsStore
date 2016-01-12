b4044e609b0cd882085b31adbd3e635516ce51db
Janaka Abeyaratna <janakacode@gmail.com>
2016-01-12 15:30:52 -0500

    initial commit

---
+++ SportsStore.WebUI/Models/ProductListViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Models
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PageInfo PageInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
